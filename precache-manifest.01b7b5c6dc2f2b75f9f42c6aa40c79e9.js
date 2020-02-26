0d0771c7d372d6140098ecc824696bbca374aa2e
Sebastian Ulloa <sebast_ulloa@hotmail.com>
2020-02-25 19:21:54 -0500

    Updates

---
+++ precache-manifest.01b7b5c6dc2f2b75f9f42c6aa40c79e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d7b63cc2347b2a277f194f463a36c5f",
+    "url": "/react-ud-monsters/index.html"
+  },
+  {
+    "revision": "5e2cfee201280b0fc5d6",
+    "url": "/react-ud-monsters/static/css/main.ea9ec7ec.chunk.css"
+  },
+  {
+    "revision": "8f058d225b8f94fee239",
+    "url": "/react-ud-monsters/static/js/2.4abd6fec.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-ud-monsters/static/js/2.4abd6fec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2cfee201280b0fc5d6",
+    "url": "/react-ud-monsters/static/js/main.239b84b5.chunk.js"
+  },
+  {
+    "revision": "84e3b3c1a1e145ef8974",
+    "url": "/react-ud-monsters/static/js/runtime-main.5dfe98e7.js"
+  }
+]);
