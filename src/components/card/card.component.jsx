cd9966151cdc1ac32a5abdc055cbedea82568426
Sebastian Ulloa <sebast_ulloa@hotmail.com>
2020-02-25 19:22:25 -0500

    intial version

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+import "./card.styles.css";
+
+class Card extends Component {
+  render() {
+    const { monster } = this.props;
+    return (
+      <div className="card-container">
+        <img
+          alt="monster"
+          src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
+        />
+        <h2> {monster.name} </h2>
+        <p>{monster.email}</p>
+      </div>
+    );
+  }
+}
+export default Card;
